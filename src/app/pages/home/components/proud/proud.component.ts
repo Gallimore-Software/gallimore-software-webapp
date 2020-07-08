343517ef30e77c86eda6b243a4555933b97b0c69
Nick Gallimore <nicholas.gallimore@rovisys.com>
2020-07-08 18:50:04 -0400

    Add static blocks

---
+++ src/app/pages/home/components/proud/proud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proud',
+  templateUrl: './proud.component.html',
+  styleUrls: ['./proud.component.scss']
+})
+export class ProudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
