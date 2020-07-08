149ef1ebd5ab58c0dff1283b5985a1f7a950781d
Nick Gallimore <nicholas.gallimore@rovisys.com>
2020-07-08 16:26:15 -0400

    Start implementing mockups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,36 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { NavComponent } from './shared/components/nav/nav.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AboutUsComponent } from './shared/components/about-us/about-us.component';
+import { NavComponent } from './shared/components/nav/nav.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ComponentPageTitle } from './shared/services/page-title/page-title';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutUsComponent,
-    NavComponent
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [ComponentPageTitle],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
