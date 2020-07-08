f81ceb3a4a4f7eae526f65eee09b47c171a9508f
Nick Gallimore <nick@gallimoresoftware.com>
2020-07-08 17:48:14 -0400

    Add github link

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { environment } from '../../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,9 +11,12 @@ export class HeaderComponent implements OnInit {
   public github = '@assets/github-circle-white-transparent.svg';
   public title = environment.title;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
   }
 
+  onGitHubClick() {
+    this.document.location.href = 'https://github.com/nfgallimore';
+  }
 }
