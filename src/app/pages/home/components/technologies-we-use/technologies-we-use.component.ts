a4180adf44c166611a11a32d872572a553a9df2f
Osman Alhassan <oalhassan847@gmail.com>
2022-10-13 19:22:32 +0000

    Added technologies-we-use section

---
+++ src/app/pages/home/components/technologies-we-use/technologies-we-use.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies-we-use',
+  templateUrl: './technologies-we-use.component.html',
+  styleUrls: ['./technologies-we-use.component.scss']
+})
+export class TechnologiesWeUseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
