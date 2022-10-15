c41183be4dff0f3166b00eaa59513aa190c750ab
Nicholas Gallimore <nfgallimore@gmail.com>
2022-10-14 23:27:05 -0400

    Update introduction

--- src/app/shared/components/slide-show/slide-show.component.ts
+++ src/app/shared/components/slide-show/slide-show.component.ts
@@ -16,31 +16,34 @@ export class SlideShowComponent implements OnInit {
   }
 
   next() {
-    (this.slideIndex === 3) ? this.slideIndex = 0 : this.slideIndex++;
+    this.slideIndex === 3 ? this.slideIndex = 0 : this.slideIndex++;
+    this.show(this.slideIndex);
   }
 
   prev() {
-    (this.slideIndex === 0) ? this.slideIndex = 3 : this.slideIndex--;
+    this.slideIndex === 0 ? this.slideIndex = 3 : this.slideIndex--;
+    this.show(this.slideIndex);
   }
 
   show(n) {
     this.slideIndex = n;
     switch (n) {
       case 0:
-        this.textContent = 'We offer high quality business supporting software solutions, for small to medium size businesses, to fit our customers\' unique needs.';
+        this.textContent = 'We offer high quality software solutions for small to medium size businesses.';
         break;
       case 1:
-        this.textContent = "No bullshit, just good quality code, written for you -- by us, with business goals in mind."
+        this.textContent = "Good quality code, written for you -- by us, with business goals in mind."
         break;
       case 2:
         this.textContent = "We would love to schedule a meeting with you today. Whatever it is, we can do it."
         break;
       case 3:
+        this.textContent = "Thank you for viewing our site. Come back anytime, updates come frequently!"
         break;
     }
   }
 
-  getBackgroundColor(n) {
+  setButtonSelected(n) {
     return n === this.slideIndex ? 'white' : 'transparent';
   }
 }
