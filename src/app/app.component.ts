dbb2da4b5e494b662fd0c7b8f432f5702a7463ec
Nicholas Gallimore <nfgallimore@gmail.com>
2022-10-14 20:42:16 -0400

    Restructure home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gallimore-software-site';
+  hideRouterOutlet = false;
+
+  onShowSidenav() {
+    if (this.hideRouterOutlet) {
+      this.hideRouterOutlet = false;
+    } else {
+      this.hideRouterOutlet = true;
+    }
+  }
 }
