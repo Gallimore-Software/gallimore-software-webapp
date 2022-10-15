7fdb94838c864e39c8a0a895c57e9ecc15f161f3
Nicholas Gallimore <nfgallimore@gmail.com>
2022-10-14 20:07:14 -0400

    Restructure navigation, add projects page

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 import { ComponentPageTitle } from '@app/shared/services/page-title/page-title';
 
 @Component({
@@ -8,11 +9,15 @@ import { ComponentPageTitle } from '@app/shared/services/page-title/page-title';
 })
 export class NavComponent {
 
-  constructor(public componentPageTitle: ComponentPageTitle) { }
+  constructor(public componentPageTitle: ComponentPageTitle, private router: Router) { }
 
   @Output() toggleSidenav = new EventEmitter<null>();
 
   getTitle() {
     return this.componentPageTitle.title;
+  }
+
+  route(page) {
+    this.router.navigate([page]);
   }
 }
