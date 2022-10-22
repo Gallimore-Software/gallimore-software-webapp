0d20094f40944ca5e348a823bd5e00e65ffe3235
Nick <nfgallimore@gmail.com>
2022-10-22 02:18:21 -0400

    Add project components

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from './models/project';
 
 @Component({
   selector: 'app-projects',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
+
+  @Input() projets : Array<Project> = [
+    new Project('Inventory Manager', 'Manager your inventory in Angular', 'https://gallimoresoftware.com/inventory-manager', ''),
+    new Project('Inventory Manager', 'Manager your inventory in Angular', 'https://gallimoresoftware.com/inventory-manager', ''),
+    new Project('Inventory Manager', 'Manager your inventory in Angular', 'https://gallimoresoftware.com/inventory-manager', '')
+  ];
 
   ngOnInit(): void {
   }
