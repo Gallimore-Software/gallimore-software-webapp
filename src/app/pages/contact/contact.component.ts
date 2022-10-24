e67ce5ca92f08b50c33d292511d88a5e8877051b
Nick <nfgallimore@gmail.com>
2022-10-24 15:59:41 -0400

    Add reactive forms module

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm = new FormGroup({
+    company: new FormControl('Company', Validators.required),
+    name: new FormControl('Company', Validators.required),
+    email: new FormControl('default@email.com', Validators.required),
+    phone: new FormControl('Company', Validators.required),
+    message: new FormControl('default@email.com', Validators.required)
+  });
 
   constructor() { }
 
