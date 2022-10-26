dbfb4855073ccf49dbd496180ffc79a18e7d1f1e
Osman Alhassan <newton@gallimoresoftware.com>
2022-10-26 02:57:36 -0700

    Angular upgraded to version 14.2.7

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:280a310ada4f09b5e1d1ed0a94e0c22f03ed0ec9df0119a4a92a24637ec19ab8
-size 818
+oid sha256:82f623af609e80add0396591e94f9bde9540f19a84af31ae2eef98fb3133dee2
+size 874
