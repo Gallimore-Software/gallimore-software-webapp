993c75b7f79ec3ea49d7d72c0396da6a8339a0a0
Osman Alhassan <newton@gallimoresoftware.com>
2022-10-31 12:20:23 +0000

    Minor contact-us styling

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:280a310ada4f09b5e1d1ed0a94e0c22f03ed0ec9df0119a4a92a24637ec19ab8
-size 818
+oid sha256:98171a2c67893bd3a9cbaad5d840e572cb624d4c150607c07649b9a5067f3101
+size 806
